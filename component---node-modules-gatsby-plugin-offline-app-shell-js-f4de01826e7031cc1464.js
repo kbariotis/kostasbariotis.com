5ba1a2f3f22f447d61f5c662bade37e7f4f05519
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-19 20:27:08 +0000

    Deploy kbariotis/kostasbariotis.com to github.com/kbariotis/kostasbariotis.com.git:gh-pages

---
+++ component---node-modules-gatsby-plugin-offline-app-shell-js-f4de01826e7031cc1464.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{288:function(e,t,n){"use strict";var u=n(5);t.__esModule=!0,t.default=void 0;var o=u(n(37)),r=u(n(1)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement("div",null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-f4de01826e7031cc1464.js.map
