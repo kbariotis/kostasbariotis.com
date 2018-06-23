c4e3ef83dae2c28b262a09c20d25873ed4708146
Kostas Bariotis <konmpar@gmail.com>
2018-06-24 01:20:28 +0300

    fix return statement

--- src/components/blog/schemas/WebPageSchema.js
+++ src/components/blog/schemas/WebPageSchema.js
@@ -4,29 +4,31 @@ import React from 'react';
 import { StaticQuery } from 'gatsby';
 
 export default function WebPageSchema() {
-  <StaticQuery
-    query={graphql`
-      query WebPageSchemaQuery {
-        site {
-          siteMetadata {
-            siteUrl
-            title
-            description
+  return (
+    <StaticQuery
+      query={graphql`
+        query WebPageSchemaQuery {
+          site {
+            siteMetadata {
+              siteUrl
+              title
+              description
+            }
           }
         }
-      }
-    `}
-    render={data => {
-      let { siteUrl, title, description } = data.site.siteMetadata;
+      `}
+      render={data => {
+        let { siteUrl, title, description } = data.site.siteMetadata;
 
-      const payload = `{
-        "@context": "http://schema.org/",
-        "@type": "WebPage",
-        "name": "${title}",
-        "url": "${siteUrl}",
-        "description": "${description}"
-      }`;
-      return <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: payload }} />;
-    }}
-  />;
+        const payload = `{
+          "@context": "http://schema.org/",
+          "@type": "WebPage",
+          "name": "${title}",
+          "url": "${siteUrl}",
+          "description": "${description}"
+        }`;
+        return <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: payload }} />;
+      }}
+    />
+  );
 }
